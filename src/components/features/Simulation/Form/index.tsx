3766cf1200072b75c87aa931451ee3c4308c197c
Filippe Melo <filippemelor@gmail.com>
2026-08-12 12:35:54 -0300

    feat: Lógica de Avançar, Voltar e Exibir Progresso

--- src/components/features/Simulation/Form/index.tsx
+++ src/components/features/Simulation/Form/index.tsx
@@ -1,14 +1,37 @@
+import { useState } from 'react'
+
 import { simulationFormSteps } from '@/data/simulation'
 
 import { FormStep } from '../FormStep'
-import { FormProgress } from '../Progress'
+import { StepProgress } from '../Progress'
 
 export const SimulationForm = () => {
-  const currentStep = simulationFormSteps[0]
+  const [currentStepIndex, setCurrentStepIndex] = useState(0)
+  const totalSteps = simulationFormSteps.length
+  const currentStep = simulationFormSteps[currentStepIndex]
+
+  const handleNextStep = () => {
+    if (currentStepIndex + 1 > totalSteps - 1) {
+      return
+    }
+
+    setCurrentStepIndex((prev) => prev + 1)
+  }
+
+  const handlePreviousStep = () => {
+    if (currentStepIndex === 0) {
+      return
+    }
+
+    setCurrentStepIndex((prev) => prev - 1)
+  }
 
   return (
     <>
-      <FormProgress currentStep={1} totalSteps={6} />
+      <StepProgress
+        currentStep={currentStepIndex + 1}
+        totalSteps={totalSteps}
+      />
       <FormStep key={currentStep.id} {...currentStep} />
     </>
   )
